141f73d7920bb863a61c5992251a3409334307ee
Nikita Khubaev <khubaev.n@yandex.ru>
2023-06-08 22:30:22 +0400

    обновил стили

--- src/components/TransactionContainer.js
+++ src/components/TransactionContainer.js
@@ -4,8 +4,8 @@ function TransactionContainer ({ transaction }) {
             <div className="transaction-container__paragraph">Дата: {transaction.date}</div>
             <div className="transaction-container__paragraph">Категория: {transaction.category}</div>
             <div className="transaction-container__paragraph">Место: {transaction.location}</div>
-            <div className="transaction-container__paragraph">Сумма в иностранной валюте: {transaction.amountForeign} {transaction.currencyForeign}</div>
-            <div className="transaction-container__paragraph">Сумма в местной валюте: {transaction.amountLocal} {transaction.currencyLocal}</div>
+            <div className="transaction-container__paragraph">Сумма в местной валюте: {transaction.amountForeign} {transaction.currencyForeign}</div>
+            <div className="transaction-container__paragraph">Эквивалент в иностранной валюте: {transaction.amountLocal} {transaction.currencyLocal}</div>
         </div>
     )
 }
