3e92fed1375fef07313a9c3590ce7f5b6e426b88
Nikita Khubaev <khubaev.n@yandex.ru>
2023-06-23 20:21:37 +0400

    поправил стили кнопки удаления в компоненте TransactionContainer

--- src/components/ExchangeRate.js
+++ src/components/ExchangeRate.js
@@ -1,10 +1,13 @@
 function ExchangeRate () {
+    const rubToUsd = 86.22; //курс рубля к доллару на 23.06.2023
+    const rubToGel = 32.71; //курс рубля к лари на 23.06.2023
+
     return (
         <div className="exchange">
             <h2 className="exchange__title">Курс валют</h2>
             <div className="exchange__container">
-                <p className="exchange__container__paragraph">USD: 84,28</p>
-                <p className="exchange__container__paragraph">GEL: 31,95</p>
+                <p className="exchange__container__paragraph">USD: {rubToUsd}</p>
+                <p className="exchange__container__paragraph">GEL: {rubToGel}</p>
             </div>
         </div>
     );
